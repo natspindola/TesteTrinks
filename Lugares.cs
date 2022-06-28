9ac693d1509c74b123f5cc8273d7372ccccd529e
Perk <natspindola@hotmail.com>
2022-06-28 20:42:13 -0300

    Lista de lugares para viajar

---
+++ Lugares.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TesteTrinks
+{
+        public class Lugares
+        {
+            public Lugares() { }
+
+            public string Nome { get; set; }
+            public int Valor { get; set; }
+
+            public Lugares(int valor, string nome)
+            {
+                this.Valor = valor;
+                this.Nome = nome;
+            }
+        }
+
+}
--- NumerosAleatorios.cs
+++ NumerosAleatorios.cs
@@ -4,14 +4,6 @@ namespace NumerosAleatórios
 {
     class NumerosAleatorios
     {
-        static void Main(string[] args)
-        {
-            Random numAleatorio = new Random();
-            int valorInteiro = numAleatorio.Next(1, 80);
-            valorInteiro.ToString();
-
-            Console.WriteLine("Número sorteado: " + valorInteiro);
-
-        }
+        
     }
 }
